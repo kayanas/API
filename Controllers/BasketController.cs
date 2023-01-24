be746a01f73d19471bbc787a5110775bee10b676
kayanas <karenauskas@gmail.com>
2023-01-24 15:09:29 +0200

    changes

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -11,69 +11,64 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class BasketContoller : BaseApiController
+    public class BasketController : BaseApiController
     {
         private readonly StoreContext _context;
-        public BasketContoller(StoreContext context)
+        public BasketController(StoreContext context)
         {
             _context = context;
             
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetBasket")]
         public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
-
             if (basket == null) return NotFound();
-
-            return new BasketDto
-            {
-                Id = basket.Id,
-                BuyerId = basket.BuyerId,
-                Items = basket.Items.Select(item => new BasketItemDto
-                {
-                    ProductId = item.ProductId,
-                    Name = item.Product.Name,
-                    Price = item.Product.Price,
-                    PictureUrl = item.Product.PictureUrl,
-                    Type = item.Product.Type,
-                    Quantity = item.Quantity
-                }).ToList()
-            };
+            
+            return MapBasketToDto(basket);
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
         {   
-            // get basket or create basket
             var basket = await RetrieveBasket();
             if(basket == null) basket = CreateBasket();
-            // get product
+
             var product = await _context.Products.FindAsync(productId);
             if (product == null) return NotFound();
-            // add item
+
             basket.AddItem(product, quantity);
-            // save changes
+
             var result = await _context.SaveChangesAsync() > 0;
-            if (result) return StatusCode(201); 
+
+            if (result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
+        
+
             return BadRequest(new ProblemDetails{Title = "Problem saving item to basket"});
         }
 
         [HttpDelete]
         public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
         {
-            // get basket
-            // remove item or reduce quantity
-            // save changes
-            return Ok();
+            var basket = await RetrieveBasket();
+
+            if ( basket == null ) return NotFound();
+
+            basket.RemoveItem(productId, quantity);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest(new ProblemDetails{Title = "Problem removing item from basket"});
         }
 
 
         private async Task<Basket> RetrieveBasket()
         {
             return await _context.Baskets
-                 .Include(i => i.Items)
+                .Include(i => i.Items)
                 .ThenInclude(p => p.Product)
                 .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
         }
@@ -85,6 +80,25 @@ namespace API.Controllers
             var basket = new Basket{BuyerId = buyerId};
             _context.Baskets.Add(basket);
             return basket;
+        }
+
+        private BasketDto MapBasketToDto(Basket basket)
+        {
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    Weight = item.Product.Weight,
+                    PictureUrl = item.Product.PictureUrl,
+                    Type = item.Product.Type,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
         }
     }
 }
--- DTOs/BasketItemDto.cs
+++ DTOs/BasketItemDto.cs
@@ -5,6 +5,7 @@ namespace API.DTOs
         public int ProductId { get; set; }
         public string Name { get; set; }
         public long Price { get; set; }
+        public double Weight { get; set; }
         public string PictureUrl { get; set; }
         public string Type { get; set; }
         public int Quantity { get; set; }
